775d115e356a21c338a532e49f22e5bb0af3b495
rosepuppy <hezhenqian@gmail.com>
2014-08-11 19:03:49 -0400

    got light sensor working

--- Hue/app/src/main/java/com/example/bill/hue/MyActivity.java
+++ Hue/app/src/main/java/com/example/bill/hue/MyActivity.java
@@ -4,14 +4,36 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.widget.TextView;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 
 public class MyActivity extends Activity {
 
+    TextView lightText, availableText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+        lightText = (TextView) findViewById(R.id.light_output);
+        availableText = (TextView) findViewById(R.id.sensor_available);
+        lightText.setText("LIGHT: ");
+
+        SensorManager mySensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+
+        Sensor LightSensor = mySensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        if(LightSensor != null){
+            availableText.setText("Sensor.TYPE_LIGHT Available");
+            mySensorManager.registerListener(
+                    LightSensorListener,
+                    LightSensor,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+
+        }else{
+            availableText.setText("Sensor.TYPE_LIGHT NOT Available");
+        }
     }
 
 
@@ -33,4 +55,22 @@ public class MyActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    private final SensorEventListener LightSensorListener
+            = new SensorEventListener(){
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            if(event.sensor.getType() == Sensor.TYPE_LIGHT){
+                lightText.setText("LIGHT: " + event.values[0]);
+            }
+        }
+
+    };
 }
